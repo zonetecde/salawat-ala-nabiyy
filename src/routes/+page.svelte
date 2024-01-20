88635a2fdafdec197faec74941e8f7642f0c3452
zonetecde <zonetecde@gmail.com>
2024-01-20 20:41:48 +0100

    main screen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="h-screen w-screen bg-purple-400 p-5 text-center flex items-center justify-center">
+	<div class="w-full flex justify-center items-center flex-col -mt-14 md:w-4/12">
+		<img src="salawat.png" alt="salawat" width="70%" />
+
+		<p>Que la paix et la bénédiction d'Allah soient sur notre Maitre Muhammad</p>
+
+		<button class="mt-5 bg-purple-700 text-white p-3 rounded-lg shadow-lg hover:bg-purple-800">
+			<a href="https://www.youtube.com/watch?v=Z3ZAGBL6UBA" target="_blank">Créer un groupe</a>
+		</button>
+
+		<p class="mt-5 text-gray-700">— ou —</p>
+
+		<button class="mt-5 bg-purple-700 text-white p-3 rounded-lg shadow-lg hover:bg-purple-800">
+			<a href="https://www.youtube.com/watch?v=Z3ZAGBL6UBA" target="_blank">Rejoindre un groupe</a>
+		</button>
+	</div>
+</div>
