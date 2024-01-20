984cf40e40d9f62cf9f48d1bdf4c4eefe57ed28e
zonetecde <zonetecde@gmail.com>
2024-01-20 20:21:16 +0100

    + database

--- src/routes/api/+server.ts
+++ src/routes/api/+server.ts
@@ -1,4 +1,11 @@
 import { sql } from '@vercel/postgres';
 
-const likes = 100;
-const { rows, fields } = await sql`SELECT * FROM posts WHERE likes > ${likes} LIMIT 5;`;
+/** @type {import('./$types').RequestHandler} */
+export async function GET({ url }: { url: URL }) {
+	try {
+		const result = await sql``;
+		return new Response(JSON.stringify({ ok: { result }, code: { status: 200 } }));
+	} catch (error) {
+		return new Response(JSON.stringify({ error: { error }, code: { status: 500 } }));
+	}
+}
